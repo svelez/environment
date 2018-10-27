9956c09254710629ee38730db7a44f042df4b718
Steven Velez <steven.velez@oqton.com>
2018-10-26 21:41:12 -0400

    Configure additional oh my zsh customizations

--- setup.sh
+++ setup.sh
@@ -3,6 +3,27 @@
 set -eo pipefail
 thisdir="$(cd "$(dirname "$0")" && pwd)"
 
+function linkto() {
+    local srcs="$1"
+    local dest="$2"
+
+    # record the full path
+    srcs=$(cd "$srcs" && pwd)
+
+    # cd to the target in a subshell
+    (
+        cd "$dest"
+        find "$srcs" -depth 1 | while read e; do
+            if [[ -e $(basename "$e") ]]; then
+                rm -rf "$(basename "$e")"
+            fi
+            ln -s "$e"
+        done
+    )
+
+}
+
+
 if [[ "$SHELL" != *"/zsh" ]]; then
     # TODO: Make sure zsh is available on the system
     chsh zsh
@@ -12,19 +33,6 @@ if [[ ! -d ~/.oh-my-zsh ]]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
-# TODO: Fetch these oh-my-zsh plugins and themes:
-# git@github.com:bhilburn/powerlevel9k.git
-# git@github.com:zsh-users/zsh-autosuggestions.git
-# git@github.com:zsh-users/zsh-syntax-highlighting.git
-
-find "$thisdir/homedir" -depth 1 | while read e; do
-    (
-        # Change to a subshell to protect current directory in case of failure
-        cd "$HOME"
-
-        if [[ -e $(basename "$e") ]]; then
-            rm -rf "$(basename "$e")"
-        fi
-        ln -s "$e"
-    )
-done
+linkto "$thisdir/homedir" "$HOME"
+linkto "$thisdir/submodules/omz/plugins" "$HOME/.oh-my-zsh/custom/plugins"
+linkto "$thisdir/submodules/omz/themes" "$HOME/.oh-my-zsh/custom/themes"
